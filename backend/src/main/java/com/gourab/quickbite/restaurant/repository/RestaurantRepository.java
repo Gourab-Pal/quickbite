c1247d58118b04bc021d7d395805554c9498cf6a
Gourab-Pal <gourab.pal.gpal@gmail.com>
2026-08-28 10:42:26 +0530

    added two more get apis for filtering

--- backend/src/main/java/com/gourab/quickbite/restaurant/repository/RestaurantRepository.java
+++ backend/src/main/java/com/gourab/quickbite/restaurant/repository/RestaurantRepository.java
@@ -1,9 +1,12 @@
 package com.gourab.quickbite.restaurant.repository;
 
 import com.gourab.quickbite.restaurant.entity.RestaurantEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
 
 public interface RestaurantRepository extends JpaRepository<RestaurantEntity, UUID> {
+    Page<RestaurantEntity> findByOpen(boolean open, Pageable pageable);
+    Page<RestaurantEntity> findByCityCode(Long cityCode, Pageable pageable);
 }
